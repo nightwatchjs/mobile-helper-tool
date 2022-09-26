cccbf4e006ffb4300ec78dab64e63fafb23cbb18
Priyansh Garg <priyanshgarg30@gmail.com>
2022-09-26 22:29:39 +0530

    Call out in help if no command passed.

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,11 @@ export const run = () => {
   try {
     const argv = process.argv.slice(2);
     const {_: args, ...options} = minimist(argv, {
-      boolean: 'install'
+      boolean: ['install', 'setup', 'help']
     });
 
     if (!args[0] || !AVAILABLE_COMMANDS.includes(args[0])) {
-      help(args[0]);
+      showHelp(args[0], options.help);
     } else if (args[0] === 'android') {
       console.log('android setup here');
     } else {
@@ -23,9 +23,11 @@ export const run = () => {
   }
 };
 
-const help = (arg: string) => {
-  if (arg) {
-    console.log(colors.red(`unknown command: ${arg}`), '\n');
+const showHelp = (cmdPassed: string, helpFlag: boolean) => {
+  if (cmdPassed) {
+    console.log(colors.red(`unknown command: ${cmdPassed}`), '\n');
+  } else if (!helpFlag) {
+    console.log(colors.red('No command passed.'), '\n');
   }
   console.log(`Available commands: ${AVAILABLE_COMMANDS}`);
   console.log(`To know more about each command, run:
