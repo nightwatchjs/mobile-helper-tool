50bbfa300dd177c3b213580a9183f92d0fdb0f09
Durgesh kumar prajapati <98798977+Durgesh4993@users.noreply.github.com>
2024-03-01 18:10:06 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,30 @@
 import colors from 'ansi-colors';
 import minimist from 'minimist';
+import { AndroidSetup } from './commands/android';
+import { IosSetup } from './commands/ios';
+import { AVAILABLE_COMMANDS } from './constants';
 
-import {AndroidSetup} from './commands/android';
-import {IosSetup} from './commands/ios';
-import {AVAILABLE_COMMANDS} from './constants';
-
-export const run = () => {
+const run = () => {
   try {
-    const argv = process.argv.slice(2);
-    const {_: args, ...options} = minimist(argv, {
+    const { _: args, ...options } = minimist(process.argv.slice(2), {
       boolean: ['install', 'setup', 'help', 'appium'],
-      alias: {
-        help: 'h',
-        mode: 'm',
-        browsers: ['b', 'browser'],
-        setup: ['install', 'i']
-      }
+      alias: { help: 'h', mode: 'm', browsers: ['b', 'browser'], setup: ['install', 'i'] }
     });
 
-    if (!args[0] || !AVAILABLE_COMMANDS.includes(args[0])) {
-      showHelp(args[0], options.help);
-    } else if (args[0] === 'android') {
-      const androidSetup = new AndroidSetup(options);
-      androidSetup.run();
-    } else {
-      const iOSSetup = new IosSetup(options);
-      iOSSetup.run();
-    }
-  } catch (err) {
-    console.error(err as string);
-    process.exit(1);
-  }
+    if (!args[0] || !AVAILABLE_COMMANDS.includes(args[0])) showHelp(args[0], options.help);
+    else (args[0] === 'android') ? new AndroidSetup(options).run() : new IosSetup(options).run();
+  } catch (err) { console.error(err), process.exit(1); }
 };
 
-const showHelp = (cmdPassed: string, helpFlag: boolean) => {
-  if (cmdPassed) {
-    console.log(colors.red(`unknown command: ${cmdPassed}`), '\n');
-  } else if (!helpFlag) {
-    console.log(colors.red('No command passed.'), '\n');
-  }
-
+const showHelp = (cmd, helpFlag) => {
+  console.log(colors.red(cmd ? `unknown command: ${cmd}` : 'No command passed.'), '\n');
   console.log(`Usage: ${colors.cyan('npx @nightwatch/mobile-helper COMMAND [options]')}\n`);
-
   console.log(`Available commands: ${colors.green(AVAILABLE_COMMANDS.join(', '))}\n`);
-  console.log(`To know more about each command, run:
-  ${colors.cyan('npx @nightwatch/mobile-helper COMMAND --help')}`);
+  console.log(`To know more about each command, run:\n${colors.cyan('npx @nightwatch/mobile-helper COMMAND --help')}`);
 };
 
-export {AndroidSetup, IosSetup};
+export { run, AndroidSetup, IosSetup };
 export * from './commands/android/adb';
-export {getBinaryNameForOS, getBinaryLocation} from './commands/android/utils/common';
-export {execBinarySync} from './commands/android/utils/sdk';
-export {getPlatformName} from './utils';
+export { getBinaryNameForOS, getBinaryLocation } from './commands/android/utils/common';
+export { execBinarySync } from './commands/android/utils/sdk';
+export { getPlatformName } from './utils';
