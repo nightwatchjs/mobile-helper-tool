0d23ba82d78aeba6b1c4b9e390c09163544c5897
itsspriyansh <priyanshkp01@gmail.com>
2024-06-26 16:20:02 +0530

    added class for dotcommands

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,3 @@
 export const AVAILABLE_COMMANDS = ['android', 'ios'];
+export const AVAILABLE_DOT_COMMANDS = ['android'];
+export const ANDROID_DOT_COMMANDS = ['adb', 'emulator', 'sdkmanager', 'avdmanager'];
