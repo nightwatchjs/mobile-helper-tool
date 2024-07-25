9e5c20bc203dc7e71202daadd57e8219c8a649a8
itsspriyansh <priyanshkp01@gmail.com>
2024-07-25 11:52:29 +0530

    show help for valued flags

--- src/commands/android/utils/common.ts
+++ src/commands/android/utils/common.ts
@@ -9,10 +9,13 @@ import path from 'path';
 import untildify from 'untildify';
 import which from 'which';
 
-import {symbols} from '../../../utils';
-import {ABI, AVAILABLE_OPTIONS, AVAILABLE_SUBCOMMANDS, DEFAULT_CHROME_VERSIONS, DEFAULT_FIREFOX_VERSION, SDK_BINARY_LOCATIONS} from '../constants';
-import {Platform, SdkBinary} from '../interfaces';
 import Logger from '../../../logger';
+import {symbols} from '../../../utils';
+import {
+  ABI, AVAILABLE_OPTIONS, AVAILABLE_SUBCOMMANDS,
+  DEFAULT_CHROME_VERSIONS, DEFAULT_FIREFOX_VERSION, SDK_BINARY_LOCATIONS
+} from '../constants';
+import {Platform, SdkBinary, Subcommand, ValuedOptions} from '../interfaces';
 
 export const getAllAvailableOptions = () => {
   const mainOptions = Object.keys(AVAILABLE_OPTIONS);
@@ -212,28 +215,109 @@ export const checkJavaInstallation = (cwd: string): boolean => {
 export const getSubcommandHelp = (): string => {
   let output = '';
 
-  output += `Usage: ${colors.cyan('npx @nightwatch/mobile-helper android [subcmd] [subcmd-options]')}\n`;
+  output += `Usage: ${colors.cyan('npx @nightwatch/mobile-helper android subcmd [subcmd-options]')}\n`;
   output += '  The following subcommands are used for different operations on Android SDK:\n\n';
   output += `${colors.yellow('Subcommands and Subcommand-Options:')}\n`;
-
-  const longest = (xs: string[]) => Math.max.apply(null, xs.map(x => x.length));
 
   Object.keys(AVAILABLE_SUBCOMMANDS).forEach(subcommand => {
     const subcmd = AVAILABLE_SUBCOMMANDS[subcommand];
     const subcmdOptions = subcmd.options?.map(option => `[--${option.name}]`).join(' ') || '';
+    const subcmdValuedOptions = generateValuedFlagsString(subcmd.valuedOptions);
 
-    output += `  ${colors.cyan(subcommand)} ${subcmdOptions}\n`;
+    // A subcommand will have boolean options to facilitate multiple workflows.
+    // If a subcommand has single workflow, then it won't have boolean options but might
+    // have valued options.
+
+    // Display the subcommand name along with boolean options or valued options in the format:
+    // subcommand [--option1] [--option2] ...
+    // OR
+    // subcommand [--valuedOption1 value] [--valuedOption2 value] ...
+    output += `  ${colors.cyan(subcommand)} ${subcmdOptions} ${subcmdValuedOptions}\n`;
     output += `  ${colors.gray(subcmd.description)}\n`;
 
-    if (subcmd.options && subcmd.options.length > 0) {
-      const optionLongest = longest(subcmd.options.map(option => `--${option.name}`));
-      subcmd.options.forEach(option => {
-        const optionStr = `--${option.name}`;
-        const optionPadding = new Array(Math.max(optionLongest - optionStr.length + 3, 0)).join('.');
-        output += `    ${optionStr} ${colors.grey(optionPadding)} ${colors.gray(option.description)}\n`;
+    // Display list of valued options for the subcommand along with description
+    if (subcmd.valuedOptions) {
+      // Generate a list of valued options with their aliases in the format:
+      // --valuedOption | -v1 | -v2 ...
+      const subcmdValuedOptionsWithAlias = getValuedOptionsWithAlias(subcmd.valuedOptions);
+
+      subcmd.valuedOptions.forEach((valOption, idx) => {
+        const optionPadding = generatePadding(subcmdValuedOptionsWithAlias, subcmdValuedOptionsWithAlias[idx].length);
+        output += `    ${subcmdValuedOptionsWithAlias[idx]} ${colors.grey(optionPadding)} ${colors.gray(valOption.description)}\n`;
       });
     }
+
+    // Display the list of boolean options for the subcommand along with description
+    output += getSubcommandOptionsHelp(subcmd);
+    output += '\n';
   });
 
   return output;
 };
+
+export const getSubcommandOptionsHelp = (subcmd: Subcommand): string => {
+  let output = '';
+
+  if (subcmd.options && subcmd.options.length > 0) {
+    // Generate a list of options along with their valued flags in the format:
+    // --option [--valuedOption1 value] [--valuedOption2 value] ...
+    const optionsWithValuedFlags = subcmd.options.map((option) => {
+      const valuedFlags = generateValuedFlagsString(option.valuedOptions);
+
+      return option.name + ' ' + valuedFlags;
+    });
+
+    subcmd.options.forEach((option, idx) => {
+      const optionStr = `--${optionsWithValuedFlags[idx]}`;
+      const optionPadding = generatePadding(optionsWithValuedFlags, optionStr.length);
+
+      output += `    ${optionStr} ${colors.grey(optionPadding)} ${colors.gray(option.description)}\n`;
+
+      if (option.valuedOptions) {
+        // Generate a list of valued options with their aliases in the format:
+        // --valuedOption | -v1 | -v2 ...
+        const valuedOptionsWithAlias = getValuedOptionsWithAlias(option.valuedOptions);
+
+        option.valuedOptions.forEach((valOption, idx) => {
+          const optionPadding = generatePadding(valuedOptionsWithAlias, valuedOptionsWithAlias[idx].length);
+          output += `        ${valuedOptionsWithAlias[idx]} ${colors.grey(optionPadding)} ${colors.gray(valOption.description)}\n`;
+        });
+      }
+    });
+  }
+
+  return output;
+};
+
+const generateValuedFlagsString = (valuedOptions: ValuedOptions[] | undefined) => {
+  // Generate a string of valued flags in the format:
+  // [--valuedOption1 value] [--valuedOption2 value] ...
+  if (!valuedOptions) {
+    return '';
+  }
+
+  let valuedFlagsStr = '';
+  valuedOptions.forEach(valOption => {
+    valuedFlagsStr += `[--${valOption.name} ${valOption.value}] `;
+  });
+
+  return valuedFlagsStr;
+};
+
+const generatePadding = (array: string[], length: number): string => {
+  const longest = (xs: string[]) => Math.max.apply(null, xs.map(x => x.length));
+  const padding = new Array(Math.max(longest(array) - length + 3, 0)).join('.');
+
+  return padding;
+};
+
+const getValuedOptionsWithAlias = (valuedOptions: ValuedOptions[]): string[] => {
+  const valuedOptionsWithAlias = valuedOptions.map(valOption => {
+    const optionAlias = valOption.alias.map(alias => `-${alias}`).join(' |');
+
+    return `--${valOption.name}` + (optionAlias ? ` | ${optionAlias}` : '');
+  });
+
+  return valuedOptionsWithAlias;
+};
+
