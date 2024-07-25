290c862da219d430cf1da59534807cc9e460461a
Priyansh Prajapati <88396544+itsspriyansh@users.noreply.github.com>
2024-07-26 02:58:31 +0530

    feat: show connected devices (#49)
    Co-authored-by: Priyansh Garg <priyanshgarg30@gmail.com>

---
+++ src/commands/android/subcommands/common.ts
@@ -0,0 +1,68 @@
+import colors from 'ansi-colors';
+
+import Logger from '../../../logger';
+import ADB from '../utils/appium-adb';
+
+const deviceStateWithColor = (state: string) => {
+  switch (state) {
+    case 'device':
+      return colors.green(state) + colors.gray(' (online)');
+    case 'offline':
+      return colors.red(state);
+    default:
+      return colors.gray(state);
+  }
+};
+
+export async function showConnectedRealDevices() {
+  try {
+    const adb = await ADB.createADB({allowOfflineDevices: true});
+    const connectedDevices = await adb.getConnectedDevices();
+    const connectedRealDevices = connectedDevices.filter((device) => {
+      return !device.udid.includes('emulator') && !device.udid.includes('_adb-tls-connect');
+    });
+
+    if (connectedRealDevices.length === 0) {
+      return true;
+    }
+
+    Logger.log(colors.bold('Connected Real Devices:'));
+
+    connectedRealDevices.forEach((device, index) => {
+      Logger.log(`  ${index+1}. udid/deviceId: ${colors.green(device.udid)} / state: ${deviceStateWithColor(device.state)}`);
+    });
+    Logger.log();
+
+    return true;
+  } catch (error) {
+    Logger.log(colors.red('Error occurred while showing connected real devices.'));
+    console.error(error);
+
+    return false;
+  }
+}
+
+export async function showConnectedEmulators() {
+  try {
+    const adb = await ADB.createADB({allowOfflineDevices: true});
+    const connectedEmulators = await adb.getConnectedEmulators();
+
+    if (connectedEmulators.length === 0) {
+      return true;
+    }
+
+    Logger.log(colors.bold('Connected Emulators:'));
+
+    connectedEmulators.forEach((emu, index) => {
+      Logger.log(`  ${index+1}. udid/deviceId: ${colors.green(emu.udid)} / state: ${deviceStateWithColor(emu.state)}`);
+    });
+    Logger.log();
+
+    return true;
+  } catch (error) {
+    Logger.log(colors.red('Error occurred while showing connected emulators.'));
+    console.error(error);
+
+    return false;
+  }
+}
--- src/commands/android/subcommands/connect/index.ts
+++ src/commands/android/subcommands/connect/index.ts
@@ -1,8 +1,11 @@
-import {connectWirelessAdb} from './wireless';
 import {Options, Platform} from '../../interfaces';
+import {showConnectedRealDevices} from '../common';
+import {connectWirelessAdb} from './wireless';
 
 export async function connect(options: Options, sdkRoot: string, platform: Platform): Promise<boolean> {
   if (options.wireless) {
+    await showConnectedRealDevices();
+
     return await connectWirelessAdb(sdkRoot, platform);
   }
 
