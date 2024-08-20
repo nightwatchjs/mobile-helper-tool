ea0ffd0ecae2f09d41af368183e13e679a529ccf
Priyansh Prajapati <88396544+itsspriyansh@users.noreply.github.com>
2024-08-20 15:56:39 +0530

    feat: Add flow for creating AVDs (#58)
    Co-authored-by: Priyansh Garg <priyanshgarg30@gmail.com>

--- src/commands/android/subcommands/install/index.ts
+++ src/commands/android/subcommands/install/index.ts
@@ -1,9 +1,12 @@
 import {Options, Platform} from '../../interfaces';
 import {installApp} from './app';
+import {createAvd} from './avd';
 
 export async function install(options: Options, sdkRoot: string, platform: Platform): Promise<boolean> {
   if (options.app) {
     return await installApp(options, sdkRoot, platform);
+  } else if (options.avd) {
+    return await createAvd(sdkRoot, platform);
   }
 
   return false;
